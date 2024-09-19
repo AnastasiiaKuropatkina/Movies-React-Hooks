1506fead612ad29c125b30356f86092a465bbb3b
Anastasiia <anastasiia@MBP-Nikolaj.lan>
2024-09-19 15:15:45 +0200

    add #

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     <div className="nav-wrapper light-blue darken-1">
       <a href="!#" className="brand-logo">React Movies</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="!#">Repo</a></li>
+        <li><a href="https://anastasiiakuropatkina.github.io/Movies-React-Hooks/">Repo</a></li>
       </ul>
     </div>
   </nav>
